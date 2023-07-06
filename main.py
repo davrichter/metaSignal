9deef889207080de932bf4b749362a5c5f8ca9ca
David Richter <david-richter-g@outlook.com>
2023-07-06 14:52:02 +0200

    Add functionality

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import datetime
+
 from pyexiv2 import Image
 import sys
+import os
 
 
 def add_metadata(path):
-    pass
+    for file in os.listdir(path):
+        filename = os.fsdecode(file)
+        if filename.endswith(".jpg"):
+            with Image(str(path + filename)) as img:
+                date = datetime.datetime.strptime((filename[7:23]), "%Y-%m-%d-%H-%M")
+
+                exif = img.read_exif()
+                exif['Exif.Photo.DateTimeOriginal'] = date.strftime('%Y:%m:%d %H:%M:%S')
+                img.modify_exif(exif)
+            continue
+        else:
+            continue
 
 
 if __name__ == "__main__":
